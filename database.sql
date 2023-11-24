302fad64bc4c83ca0e60b13409deef703f6cafb7
Sakthivel Madhu <sakthi130597@gmail.com>
2023-11-24 20:01:34 +0530

    done db

--- database.sql
+++ database.sql
@@ -2,10 +2,9 @@
 CREATE DATABASE IF NOT EXISTS your_database_name;
 USE your_database_name;
 
--- Create a table for users
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
     mobile VARCHAR(15),
     latitude DECIMAL(10, 8),
     longitude DECIMAL(11, 8),
@@ -13,18 +12,17 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Create a table for products
-CREATE TABLE IF NOT EXISTS products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    product_name VARCHAR(255) NOT NULL,
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(255),
     product_description TEXT,
-    product_images JSON, -- assuming you store image paths in JSON format
-    product_price DECIMAL(10, 2) NOT NULL,
+    product_images JSON,
+    product_price DECIMAL(10, 2),
+    compressed_product_images JSON,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    compressed_product_images JSON -- assuming you store compressed image paths in JSON format
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
 
 -- Create an index on the user_id column in the products table for faster retrieval
 CREATE INDEX idx_user_id ON products (user_id);
